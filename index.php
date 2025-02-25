11c2aeb722077dd31689ebf378f0b4f00ef6a298
Aaron John Bitara <118180256+eyron29@users.noreply.github.com>
2025-02-25 18:51:04 +0800

    feat: subsription page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form-container">
+            <form action="handler.php" method="POST"></form>
+            <h1>Get FREE Updates</h1>
+            <p>Join us for FREE to get email updates!</p>
+
+            <div class=input-fields>
+            <label for="">Name:</label></br>
+            <input type="text" name="fullName" placeholder="Full Name"></br>
+
+            <label for="">Email:</label></br>
+            <input type="email" name="email" placeholder="Email Address"></br>
+            </div>
+
+            <input type="submit" value="SUBSCRIBE">
+            
+        </div>
+    </div>
+    
+</body>
+</html>
